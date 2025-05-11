7b0f3091873c64689fdd6df5e312d3484805bc22
lamijazukan <99195874+lamijazukan@users.noreply.github.com>
2025-05-11 11:50:03 +0200

    adding dockerfile

--- src/services/ApiService.tsx
+++ src/services/ApiService.tsx
@@ -1,17 +1,18 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://localhost:7124/api/v1',
-  });
-  
-  export const getFoods = async () => {
-    const response = await api.get('/foods');
-    return response;
-  };
+  baseURL: "http://localhost:5000/api/v1",
+});
 
-  export const createFood = async (newFood: { name: string; calories: number }) => {
-    const response = await api.post('/foods', newFood);
-    return response.data;
-  };
-  
+export const getFoods = async () => {
+  const response = await api.get("/foods");
+  return response;
+};
+
+export const createFood = async (newFood: {
+  name: string;
+  calories: number;
+}) => {
+  const response = await api.post("/foods", newFood);
+  return response.data;
+};
