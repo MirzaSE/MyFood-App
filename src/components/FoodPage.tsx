a88e20d133eb8241d7ea4644f42c15005f4c52d3
AjdinDervic <ajdindervic03@gmail.com>
2025-05-14 23:53:34 +0200

    Implemented login and register

--- src/components/FoodPage.tsx
+++ src/components/FoodPage.tsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { Table, TableHead, TableBody, TableRow, TableCell, Modal, Box,
   TextField,
-  Typography, 
+  Typography,
   Grid,
   Button,
   FormControl,
   FormHelperText,
-   } from '@mui/material';
+} from '@mui/material';
 import { Pagination, TableContainer  } from '@mui/material';
 import { getFoods, createFood } from '../services/ApiService'; // Assuming ApiService is in services folder
 import { useForm } from 'react-hook-form'; // Form validation library
 import styles from './FoodPage.module.css'
+import {useNavigate} from "react-router-dom";
 
 
 interface FoodItem {
@@ -25,16 +26,16 @@ function FoodPage() {
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(10);
   const [isLoading, setIsLoading] = useState(false); // Track loading state
-  const [error, setError] = useState<string | null>(null); 
+  const [error, setError] = useState<string | null>(null);
 
-    // Modal state and handlers
-    const [openAddFoodModal, setOpenAddFoodModal] = useState(false);
-    const handleOpenAddFoodModal = () => setOpenAddFoodModal(true);
-    const handleCloseAddFoodModal = () => setOpenAddFoodModal(false);
-  
-    // New food data state
-    const [newFoodName, setNewFoodName] = useState('');
-    const [newFoodCalories, setNewFoodCalories] = useState(0);
+  // Modal state and handlers
+  const [openAddFoodModal, setOpenAddFoodModal] = useState(false);
+  const handleOpenAddFoodModal = () => setOpenAddFoodModal(true);
+  const handleCloseAddFoodModal = () => setOpenAddFoodModal(false);
+
+  // New food data state
+  const [newFoodName, setNewFoodName] = useState('');
+  const [newFoodCalories, setNewFoodCalories] = useState(0);
   // Fetch data from ApiService on component mount
   useEffect(() => {
     const fetchData = async () => {
@@ -56,158 +57,161 @@ function FoodPage() {
 
   const handlePageChange = (event: React.ChangeEvent<unknown>, newPage: number) => {
     setCurrentPage(newPage);
- };
+  };
 
   // Pagination logic
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentFoods = foods.slice(indexOfFirstItem, indexOfLastItem);
+  const navigate = useNavigate();
+  // Form validation with React Hook Form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FoodItem>({ mode: 'all' }); // Validate all fields on submit
 
-    // Form validation with React Hook Form
-    const {
-      register,
-      handleSubmit,
-      formState: { errors },
-    } = useForm<FoodItem>({ mode: 'all' }); // Validate all fields on submit
-  
-    // Handle adding a new food item
-    const handleAddFood = async (data : FoodItem) => {
-      console.log(`Submitted`);
-      if (!data.name || data.calories <= 0) {
-        console.log(data.name)
-        alert('Please enter a valid food name and calorie count.');
-        return;
-      }
-      
-      // Implement logic to send newFood data to your API for creation
-      // (replace with your actual API call)
-      try {
-        const response = await createFood(data);
-        console.log(response)
-        setFoods([...foods, response]); // Update local state
-        setNewFoodName('');
-        setNewFoodCalories(0);
-        handleCloseAddFoodModal();
-      } catch (error) {
-        console.error('Error adding food:', error);
-        alert('Failed to add food item. Please try again later.');
-      }
-    };
+  // Handle adding a new food item
+  const handleAddFood = async (data : FoodItem) => {
+    console.log(`Submitted`);
+    if (!data.name || data.calories <= 0) {
+      console.log(data.name)
+      alert('Please enter a valid food name and calorie count.');
+      return;
+    }
 
-    
+    // Implement logic to send newFood data to your API for creation
+    // (replace with your actual API call)
+    try {
+      const response = await createFood(data);
+      console.log(response)
+      setFoods([...foods, response]); // Update local state
+      setNewFoodName('');
+      setNewFoodCalories(0);
+      handleCloseAddFoodModal();
+    } catch (error) {
+      console.error('Error adding food:', error);
+      alert('Failed to add food item. Please try again later.');
+    }
+  };
+
+
 
   return (
-    <div>
-      <h1>Food List</h1>
-      {isLoading && <p>Loading food data...</p>}
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      {!isLoading && !error && (
-        <>
-        <Grid container spacing={2}> {/* Adjust spacing value as needed */}
-  <Grid item xs={12}>
-        <Button variant="contained" onClick={handleOpenAddFoodModal} color="primary" className="green-button">
-            Add Food
-          </Button>
-          <br></br>
-          </Grid>
-  <Grid item xs={12}>
-          <TableContainer  sx={{ width: '80%' }}> {/* Wrap the Table component */}
-          <Table className={styles.foodTable}>
-            <TableHead>
-              <TableRow>
-                <TableCell>ID</TableCell>
-                <TableCell>Name</TableCell>
-                <TableCell>Calories</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {currentFoods.map((food) => (
-                <TableRow key={food.id}>
-                  <TableCell>{food.id}</TableCell>
-                  <TableCell>{food.name}</TableCell>
-                  <TableCell>{food.calories}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-          </TableContainer>
-          </Grid>
-</Grid>
-          <Pagination
-            count={Math.ceil(foods.length / itemsPerPage)}
-            page={currentPage}
-            onChange={handlePageChange}
-          />
-     <Modal
-        open={openAddFoodModal}
-        onClose={handleCloseAddFoodModal}
-        aria-labelledby="modal-title"
-        aria-describedby="modal-description"
-        sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} // Centering styles
+      <div>
+        <h1>Food List</h1>
+        {isLoading && <p>Loading food data...</p>}
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        {!isLoading && !error && (
+            <>
+              <Button onClick={()=>{localStorage.removeItem("accessToken")
+                navigate("/login")
+              }}>Logout</Button>
+              <Grid container spacing={2}> {/* Adjust spacing value as needed */}
+                <Grid item xs={12}>
+                  <Button variant="contained" onClick={handleOpenAddFoodModal} color="primary" className="green-button">
+                    Add Food
+                  </Button>
+                  <br></br>
+                </Grid>
+                <Grid item xs={12}>
+                  <TableContainer  sx={{ width: '80%' }}> {/* Wrap the Table component */}
+                    <Table className={styles.foodTable}>
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>ID</TableCell>
+                          <TableCell>Name</TableCell>
+                          <TableCell>Calories</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {currentFoods.map((food) => (
+                            <TableRow key={food.id}>
+                              <TableCell>{food.id}</TableCell>
+                              <TableCell>{food.name}</TableCell>
+                              <TableCell>{food.calories}</TableCell>
+                            </TableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                </Grid>
+              </Grid>
+              <Pagination
+                  count={Math.ceil(foods.length / itemsPerPage)}
+                  page={currentPage}
+                  onChange={handlePageChange}
+              />
+              <Modal
+                  open={openAddFoodModal}
+                  onClose={handleCloseAddFoodModal}
+                  aria-labelledby="modal-title"
+                  aria-describedby="modal-description"
+                  sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} // Centering styles
 
-      >
-        <Box sx={{ width: '50%', mx: 'auto', bgcolor: 'background.paper', p: 4 }}>
-          <Typography variant="h5" id="modal-title">
-            Add Food
-          </Typography>
-          <form onSubmit={handleSubmit(handleAddFood)}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <FormControl fullWidth>
-                  <TextField
-                    {...register('name', { required: 'Please enter a food name.' })}
-                    error={!!errors.name} // Set error state based on validation
-                    helperText={errors?.name ? errors?.name?.message?.toString() : 'Please enter a food name.'} 
-                    id="food-name"
-                    label="Food Name"
-                    variant="standard"
-                  />
-                </FormControl>
-              </Grid>
-              <Grid item xs={12}>
-                <FormControl fullWidth>                  
-                  <TextField
-                    {...register('calories', {
-                      required: 'Please enter calories.',
-                      validate: (value) => value > 0 || 'Calories must be positive.',
-                    })}
-                    error={!!errors.calories} // Set error state based on validation
-                    helperText={errors?.calories ? errors?.calories?.message?.toString() : 'Please enter calories.'} 
-                    id="calories"
-                    label="Calories"
-                    variant="standard"
-                    type="number"
-                  />
-                  <FormHelperText sx={{ color: 'red' }}>
-                   
-                  </FormHelperText>
-                </FormControl>
-              </Grid>
-              <Grid item xs={12}>
-                <FormControl fullWidth>
-                  <TextField
-                    {...register('type', { required: 'Please enter a type.' })}
-                    error={!!errors.name} // Set error state based on validation
-                    helperText={errors?.name ? errors?.name?.message?.toString() : 'Please enter a food type.'} 
-                    id="type-name"
-                    label="Type"
-                    variant="standard"
-                  />
-                </FormControl>
-              </Grid>
-              
-              <Grid item xs={12}>
-                <Button variant="contained" type="submit">
-                  Add
-                </Button>
-              </Grid>
-            </Grid>
-          </form>
-        </Box>
-      </Modal>
-        </>
-      )}
-    </div>
+              >
+                <Box sx={{ width: '50%', mx: 'auto', bgcolor: 'background.paper', p: 4 }}>
+                  <Typography variant="h5" id="modal-title">
+                    Add Food
+                  </Typography>
+                  <form onSubmit={handleSubmit(handleAddFood)}>
+                    <Grid container spacing={2}>
+                      <Grid item xs={12}>
+                        <FormControl fullWidth>
+                          <TextField
+                              {...register('name', { required: 'Please enter a food name.' })}
+                              error={!!errors.name} // Set error state based on validation
+                              helperText={errors?.name ? errors?.name?.message?.toString() : 'Please enter a food name.'}
+                              id="food-name"
+                              label="Food Name"
+                              variant="standard"
+                          />
+                        </FormControl>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <FormControl fullWidth>
+                          <TextField
+                              {...register('calories', {
+                                required: 'Please enter calories.',
+                                validate: (value) => value > 0 || 'Calories must be positive.',
+                              })}
+                              error={!!errors.calories} // Set error state based on validation
+                              helperText={errors?.calories ? errors?.calories?.message?.toString() : 'Please enter calories.'}
+                              id="calories"
+                              label="Calories"
+                              variant="standard"
+                              type="number"
+                          />
+                          <FormHelperText sx={{ color: 'red' }}>
+
+                          </FormHelperText>
+                        </FormControl>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <FormControl fullWidth>
+                          <TextField
+                              {...register('type', { required: 'Please enter a type.' })}
+                              error={!!errors.name} // Set error state based on validation
+                              helperText={errors?.name ? errors?.name?.message?.toString() : 'Please enter a food type.'}
+                              id="type-name"
+                              label="Type"
+                              variant="standard"
+                          />
+                        </FormControl>
+                      </Grid>
+
+                      <Grid item xs={12}>
+                        <Button variant="contained" type="submit">
+                          Add
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  </form>
+                </Box>
+              </Modal>
+            </>
+        )}
+      </div>
   );
 }
 
